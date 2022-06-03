1275d5fc16f29e9ec1e95911826cb3e9f4d6c918
Rebecka <dymllr@gmail.com>
2022-06-03 20:34:05 +0800

    mallari_assignment81.sh

--- mallari__assignment81.sh
+++ mallari__assignment81.sh
@@ -1,1 +1,46 @@
-sample lang to
+cd /opt/
+
+systemctl stop httpd
+
+vi lamp.sh
+
+#!/bin/sh
+
+# install httpd
+yum install -y httpd
+
+# start httpd service
+systemctl start httpd
+
+# enable httpd as a service
+systemctl enable httpd
+
+firewall-cmd --permanent --add-port=80/tcp
+
+firewall-cmd --permanent --add-port=443/tcp
+
+firewall-cmd --reload
+
+systemctl enable httpd
+
+yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+
+yum install -y yum-utils
+
+yum-config-manager --enable remi-php56
+
+yum install -y php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo
+
+systemctl restart httpd
+
+yum install -y mariadb-server mariadb
+
+systemctl start mariadb
+
+mysql_secure_installation
+
+systemctl enable mariadb
+
+mysql -u root -p
